e41d0fc1439d750773abd913d58745a0254ec9db
Timothy Karani <kartimothy@gmail.com>
2022-05-27 13:24:03 +0300

    Tracking

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
       </div>
     </div>
 
-    @auth
+    @if (Auth::user()->email == 'admin@elimishawatoto.com')
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-3">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
           <div class="p-6 bg-white border-b border-gray-200">
@@ -49,6 +49,6 @@
           </div>
         </div>
       </div>
-    @endauth
+    @endif
   </div>
 </x-app-layout>
