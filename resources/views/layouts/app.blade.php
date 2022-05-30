d7b881de524804b9de51542db2fb4bf467f16344
Timothy Karani <kartimothy@gmail.com>
2022-05-30 13:31:43 +0300

    Done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,12 +13,12 @@
     href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
   <!-- Styles -->
-  <link rel="stylesheet" href="{{ url(mix('css/app.css')) }}">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
   @livewireStyles
 
   <!-- Scripts -->
-  <script src="{{ url(mix('js/app.js')) }}" defer></script>
+  <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 
 <body class="font-sans antialiased">
