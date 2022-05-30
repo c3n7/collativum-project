c57bff0053da968156cf2ce96d445756ec613b10
Timothy Karani <kartimothy@gmail.com>
2022-05-30 11:01:57 +0300

    Can add user

--- app/Http/Livewire/Auth/Users.php
+++ app/Http/Livewire/Auth/Users.php
@@ -3,23 +3,67 @@
 namespace App\Http\Livewire\Auth;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Str;
 
 class Users extends Component
 {
   use WithPagination;
   public $search_term;
 
+  public $email, $name;
+
+
+  public $addingItemToModel = false;
+
+  protected $rules = [
+    'name' => 'required|max:255',
+    'email' => 'required|email|max:255|unique:users,email'
+  ];
+
   public function render()
   {
     $users = User::when(!empty($this->search_term), function ($q) {
       return $q->where('name', 'like', '%' . $this->search_term . '%');
     })
+      ->orderByDesc('id')
       ->paginate(10);
     return view(
       'livewire.auth.users',
       ["users" => $users]
     );
+  }
+
+
+  public function confirmAddingItem()
+  {
+    $this->addingItemToModel = true;
+  }
+
+  public function saveNewRecord()
+  {
+    $fields = $this->validate();
+
+    $password = Str::random(8);
+    $fields['password'] = Hash::make($password);
+    Log::info("User Created", ["user" => $fields, "password" => $password]);
+
+    $user = User::create(
+      [
+        'name' => $fields['name'],
+        'email' => $fields['email'],
+        'password' => $fields['password'],
+      ]
+    );
+
+    $this->addingItemToModel = false;
+    return redirect()->route('auth.user-management')
+      ->with('flash.banner', 'User added successfully')
+      ->with('flash.bannerStyle', 'success');
+
+    // Mail::to($user)->send(new UserRegistered($user, $password));
   }
 }
--- resources/views/livewire/auth/users.blade.php
+++ resources/views/livewire/auth/users.blade.php
@@ -1,5 +1,5 @@
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-  <div class="text-2xl">
+  <div class="text-2xl flex justify-between">
     Users
   </div>
 
@@ -7,9 +7,18 @@
 
     <!-- Name -->
     <div class="col-span-6 sm:col-span-4 mb-3">
-      <x-jet-input type="search" class="mt-1 block w-fit"
-        wire:model.debounce.250ms="search_term" placeholder="Search" />
-      <x-jet-input-error for="search_term" class="mt-2" />
+      <div class="flex justify-between items-center">
+        <div>
+          <x-jet-input type="search" class="mt-1 block w-fit"
+            wire:model.debounce.250ms="search_term" placeholder="Search" />
+          <x-jet-input-error for="search_term" class="mt-2" />
+        </div>
+        <div>
+          <x-jet-button wire:click="confirmAddingItem">
+            {{ __('Add User') }}
+          </x-jet-button>
+        </div>
+      </div>
     </div>
 
     <table class="table-auto w-full">
@@ -36,4 +45,37 @@
 
   <div class="mt-4">{{ $users->links() }}</div>
 
+  <x-jet-dialog-modal wire:model="addingItemToModel">
+    <x-slot name="title">
+      {{ __('Add User') }}
+    </x-slot>
+
+    <x-slot name="content">
+      <div class="col-span-6 sm:col-span-4">
+        <x-jet-label for="name" value="{{ __('Name') }}" />
+        <x-jet-input id="name" type="text" class="mt-1 block w-full"
+          wire:model.defer="name" autocomplete="name" />
+        <x-jet-input-error for="name" class="mt-2" />
+      </div>
+
+      <div class="col-span-6 sm:col-span-4 mt-3">
+        <x-jet-label for="email" value="{{ __('Email') }}" />
+        <x-jet-input id="email" type="email" class="mt-1 block w-full"
+          wire:model.defer="email" autocomplete="email" />
+        <x-jet-input-error for="email" class="mt-2" />
+      </div>
+    </x-slot>
+
+    <x-slot name="footer">
+      <x-jet-secondary-button wire:click="$toggle('addingItemToModel')"
+        wire:loading.attr="disabled">
+        {{ __('Cancel') }}
+      </x-jet-secondary-button>
+
+      <x-jet-button class="ml-3" wire:click="saveNewRecord"
+        wire:loading.attr="disabled">
+        {{ __('Save') }}
+      </x-jet-button>
+    </x-slot>
+  </x-jet-dialog-modal>
 </div>
